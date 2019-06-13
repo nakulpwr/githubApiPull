55eda15cb8d612bca17e824dbffb123cec3e857e
nakul_ws <nakul.wonderslate@gmail.com>
2019-06-13 16:48:03 +0530

    Added Glide module class.

---
+++ app/src/main/java/com/nakul/meesho/view/MyAppGlideModule.java
@@ -0,0 +1,8 @@
+package com.nakul.meesho.view;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public final class MyAppGlideModule extends AppGlideModule {
+}
