ab1d0cc3618f550556b81dae5eebc4517315a51c
nakul_ws <nakul.wonderslate@gmail.com>
2019-06-13 16:46:12 +0530

    Added required fields for repo model

--- app/src/main/java/com/nakul/meesho/models/RepoPullModelClass.java
+++ app/src/main/java/com/nakul/meesho/models/RepoPullModelClass.java
@@ -5,7 +5,7 @@ import android.support.v7.util.DiffUtil;
 
 public class RepoPullModelClass {
 
-    private String avatarUrl, userName, title, id;
+    private String avatarUrl, userName, title, id, issueUrl;
 
     public String getAvatarUrl() {
         return avatarUrl;
@@ -19,11 +19,42 @@ public class RepoPullModelClass {
         return title;
     }
 
+    public RepoPullModelClass() {
+    }
 
-    public RepoPullModelClass(String userName, String avatarUrl, String id) {
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getIssueUrl() {
+        return issueUrl;
+    }
+
+    public void setIssueUrl(String issueUrl) {
+        this.issueUrl = issueUrl;
+    }
+
+    public RepoPullModelClass(String userName, String avatarUrl, String id, String title) {
         this.avatarUrl = avatarUrl;
         this.userName = userName;
         this.id = id;
+        this.title = title;
     }
 
     public static DiffUtil.ItemCallback<RepoPullModelClass> DIFF_CALLBACK = new DiffUtil.ItemCallback<RepoPullModelClass>() {
